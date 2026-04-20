0d7b779bb6c0f71870ca6d77cb5a7fc5572abbff
Alexander Nollet <alexanderacj13@gmail.com>
2026-04-20 16:51:29 +0200

    beginning of the cli tool!

--- cli/go.mod
+++ cli/go.mod
@@ -1,3 +1,5 @@
 module github.com/alikznollet/godot-addon-updater
 
 go 1.26.2
+
+require github.com/alecthomas/kong v1.15.0 // indirect
--- cli/main.go
+++ cli/main.go
@@ -1,7 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/alecthomas/kong"
+)
+
+// -- Command Structs -- //
+
+// Install
+
+type InstallCmd struct {
+	Repo    string `arg:"" name:"repo" help:"The GitHub repository (e.g. ramokz/phantom-camera)."`
+	Version string `short:"v" default:"latest" help:"Specific version tag to install."`
+}
+
+func (cmd *InstallCmd) Run() error {
+	fmt.Printf("Installing %s\n", cmd.Repo)
+	fmt.Printf("Requested Version: %s\n", cmd.Version)
+
+	return nil
+}
+
+// Update
+
+type UpdateCmd struct {
+	Name string `short:"n" default:"all" help:"Specific addon to update."`
+}
+
+func (cmd *UpdateCmd) Run() error {
+	if cmd.Name == "all" {
+		fmt.Println("Attempting to update all installed addons...")
+	} else {
+		fmt.Printf("Attempting to update %s\n", cmd.Name)
+	}
+
+	return nil
+}
+
+// Init
+
+type InitCmd struct {
+	Force bool `short:"f" help:"Overwrites existing addons.json."`
+}
+
+func (cmd *InitCmd) Run() error {
+	fmt.Println("Initializing addons.json...")
+	fmt.Printf("Force Overwrite: %v\n", cmd.Force)
+
+	return nil
+}
+
+// CLI
+
+var cli struct {
+	Install InstallCmd `cmd:"" help:"Install a new addon from GitHub."`
+	Update  UpdateCmd  `cmd:"" help:"Check for updates for all installed addons."`
+	Init    InitCmd    `cmd:"" help:"Initialize a new addons.json file."`
+}
 
 func main() {
-	fmt.Println("Godot Addon Updater CLI is running!")
+	ctx := kong.Parse(
+		&cli,
+		kong.Name("godot-addon-updater"),
+		kong.Description("A CLI tool to manage Godot addons from GitHub."),
+		kong.UsageOnError(),
+	)
+
+	err := ctx.Run()
+	ctx.FatalIfErrorf(err)
 }

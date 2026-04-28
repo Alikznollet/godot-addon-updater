b67b4f4f4681749f539bb16079ec6e36fd2c1108
Alexander Nollet <alexanderacj13@gmail.com>
2026-04-28 18:12:58 +0200

    Updating now also works!!

--- cli/internal/util/util.go
+++ cli/internal/util/util.go
@@ -1,10 +1,14 @@
 package util
 
 import (
+	"bufio"
 	"errors"
+	"fmt"
 	"os"
+	"strings"
 )
 
+// Ensures the user is running the tool in a godot project.
 func EnsureGodotProject() error {
 	_, err := os.Stat("project.godot")
 
@@ -19,4 +23,33 @@ func EnsureGodotProject() error {
 	}
 
 	return nil
+}
+
+// Prompts the user with a yes/no question and waits for confirmation.
+func AskForConfirmation(prompt string) bool {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Printf("%s [y/N]: ", prompt)
+
+		// Read until enter
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			return false
+		}
+
+		// Clean up the input
+		input = strings.TrimSpace(strings.ToLower(input))
+
+		// Default to no.
+		if input == "" || input == "n" || input == "no" {
+			return false
+		}
+		if input == "y" || input == "yes" {
+			return true
+		}
+
+		// If anything else was typed loop back.
+		fmt.Println("Please type 'y' for yes or 'n' for no.")
+	}
 }
--- cli/main.go
+++ cli/main.go
@@ -198,7 +198,7 @@ func (cmd *UninstallCmd) Run() error {
 
 type UpdateCmd struct {
 	Repos []string `arg:"" optional:"" name:"repos" help:"List of all specific addons to update."`
-	Yes   bool     `short:"k" help:"Automatically confirm each update without user interaction."`
+	Yes   bool     `short:"y" help:"Automatically confirm each update without user interaction."`
 }
 
 func (cmd *UpdateCmd) Run() error {
@@ -207,14 +207,81 @@ func (cmd *UpdateCmd) Run() error {
 	}
 
 	// Load the manifest.
-	_, err := manifest.LoadManifest()
+	m, err := manifest.LoadManifest()
 	if err != nil {
 		return err
 	}
 
+	var reposToGet []manifest.Addon
 	if len(cmd.Repos) == 0 {
-		fmt.Println("Attempting to update ALL addons...")
+		reposToGet = make([]manifest.Addon, 0, len(m.Addons))
+		for _, addon := range m.Addons {
+			if !addon.Untracked {
+				reposToGet = append(reposToGet, addon)
+			}
+		}
+	} else {
+		reposToGet = make([]manifest.Addon, 0, len(cmd.Repos))
+		for _, repoName := range cmd.Repos {
+			_, addon, isTracked := m.FindByRepo(repoName)
+			if isTracked {
+				reposToGet = append(reposToGet, addon)
+			}
+		}
 	}
+
+	fmt.Printf("Attempting to update %d tracked addons.\n", len(reposToGet))
+
+	// Check and update repos that need it.
+	for _, addon := range reposToGet {
+		fmt.Printf("Attempting to update %s...\n", addon.Repo)
+
+		isUpToDate, ref, err := m.CheckAddon(addon.Repo)
+		if err != nil {
+			fmt.Printf("Error while checking %s: %v\n", addon.Repo, err)
+			continue
+		}
+
+		// Only try to update if the addon is not up to date and ref is there.
+		if !isUpToDate && ref != nil {
+			fmt.Printf("Update found for %s -> %s.\n", addon.Repo, ref.GetVersion())
+
+			// Only ask for confirm if yes is not passed.
+			if !cmd.Yes {
+				confirmationString := fmt.Sprintf("Do you want to download and apply updates for %s?", addon.Repo)
+				if !util.AskForConfirmation(confirmationString) {
+					fmt.Println("Update cancelled.")
+					continue
+				}
+			}
+
+			fmt.Println("Applying updates...")
+			loc, err := github.DownloadAndExtract(ref.GetZipballUrl())
+			if err != nil {
+				fmt.Printf("Error downloading %s: %v\n", addon.Repo, err)
+				continue // Continue to the next update.
+			}
+
+			switch addon.Type {
+			case manifest.Release:
+				m.AddRelease(loc, addon.Repo, ref.GetVersion())
+			case manifest.Branch:
+				m.AddBranch(loc, addon.Repo, addon.Version, ref.GetVersion())
+			default:
+				continue
+			}
+		} else {
+			fmt.Printf("%s is up to date.\n", addon.Repo)
+		}
+	}
+
+	// Make sure to save the manifest after all of this.
+	err = manifest.SaveManifest(m)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Finished updating addons!")
 
 	return nil
 }
